b5f08f16ddf1419c63f4a337e1fbcba72ee5a3fd
jere7612 <70173572+jere7612@users.noreply.github.com>
2024-10-23 21:03:18 -0500

    Carhga inicial de mi proyecto personal

---
+++ src/main/java/pe/edu/cibertec/proyecto_frontend_jeremias/ProyectoFrontendJeremiasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.proyecto_frontend_jeremias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFrontendJeremiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFrontendJeremiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/proyecto_frontend_jeremias/ProyectoFrontendJeremiasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.proyecto_frontend_jeremias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFrontendJeremiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
